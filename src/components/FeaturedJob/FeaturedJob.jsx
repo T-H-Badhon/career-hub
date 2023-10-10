24043f252954f3512904e67e69a0da1eb963dad6
TanvirhCSE17 <tanvirhcse17@gmail.com>
2023-10-10 13:51:07 +0600

    showMore&Less button added homepage polished

--- src/components/FeaturedJob/FeaturedJob.jsx
+++ src/components/FeaturedJob/FeaturedJob.jsx
@@ -1,10 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { JobsContext } from '../Layout/Layout';
 import JobItem from '../JobItem/JobItem';
 
 const FeaturedJob = () => {
-    const jobs=useContext(JobsContext);
-    console.log(jobs)
+    const contextjobs=useContext(JobsContext);
+    const [jobs,setJobs]=useState(contextjobs.slice(0,4))
+    
+    const showMore=()=>{
+        const newJobs=contextjobs;
+        setJobs(newJobs)
+    }
+    const showLess=()=>{
+        const newJobs=contextjobs.slice(0,4);
+        setJobs(newJobs)
+    }
+
+    
     return (
         <div className='container mx-auto my-20'>
             <h1 className='text-6xl text-violet-500 text-center my-10'>Featured Job</h1>
@@ -14,7 +25,9 @@ const FeaturedJob = () => {
                     jobs.map(job=> <JobItem key={job.id} job={job}></JobItem> )
                 }
             </div>
-            <h1 className='text-center mt-10'><button className='px-3 py-2 bg-indigo-100 rounded-lg'>Show All</button></h1>
+            <h1 className={`text-center mt-10 ${jobs.length!==4 && 'hidden'}`}><button className='px-3 py-2 bg-indigo-100 rounded-lg' onClick={showMore}>Show All</button></h1>
+            <h1 className={`text-center mt-10 ${jobs.length==4 && 'hidden'}`}><button className='px-3 py-2 bg-indigo-100 rounded-lg' onClick={showLess}>Show Less</button></h1>
+            
         </div>
     );
 };
--- src/components/JobItem/JobItem.jsx
+++ src/components/JobItem/JobItem.jsx
@@ -3,19 +3,19 @@ import { CurrencyDollarIcon, MapPinIcon } from '@heroicons/react/24/solid'
 
 const JobItem = ({job}) => {
     return (
-        <div className='border-2 bg-indigo-100 rounded-lg p-10'>
-            <img src={job.logo} alt="" />
-            <h1>{job.job_title}</h1>
-            <h1>{job.company_name}</h1>
-            <div className='flex'>
-                <button className='bg-white rounded-lg border-2 px-3 py-2 mr-3'>{job.remote_or_onsite}</button>
-                <button className='bg-white rounded-lg border-2 px-3 py-2 '>{job.job_type}</button>
+        <div className='border-2 bg-indigo-50 rounded-lg p-10'>
+            <img className='mb-5' src={job.logo} alt="" />
+            <h1 className='text-4xl mb-2'>{job.job_title}</h1>
+            <h1 className='text-2xl mb-2'>{job.company_name}</h1>
+            <div className='flex '>
+                <button className='bg-white text-indigo-500 border-indigo-100 rounded-lg border-2 px-3 py-2 mr-3'>{job.remote_or_onsite}</button>
+                <button className='bg-white text-indigo-500 border-indigo-100 rounded-lg border-2 px-3 py-2 '>{job.job_type}</button>
             </div>
-            <div className='flex'>
+            <div className='flex my-4'>
                 <h1 className='mr-5'> <span><MapPinIcon className="inline h-6 w-6 text-green-500"/></span> {job.location}</h1>
                 <h1 className='mr-5'> <span><CurrencyDollarIcon className="inline h-6 w-6 text-yellow-400"/></span> Salary: {job.salary}</h1>
             </div>
-            <button className='p-3 border-2 bg-white rounded-lg'>View Details</button>
+            <button className='p-3 border-2 bg-indigo-300 rounded-lg text-white'>View Details</button>
         </div>
     );
 };
